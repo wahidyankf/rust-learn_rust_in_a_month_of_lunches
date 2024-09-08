8e4537a88754daf7d127a4acf67b146ef2317b90
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2024-09-08 08:49:59 +0700

    go until mutable references

--- ch02_memory_variables_and_ownership/src/main.rs
+++ ch02_memory_variables_and_ownership/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let mut my_number = 8;
+    let num_ref = &mut my_number;
+    *num_ref += 10;
+    println!("{}", my_number);
+
+    let second_number = 800;
+    let triple_reference = &&&second_number;
+    println!("Are they equal? {}", second_number == ***triple_reference);
 }
